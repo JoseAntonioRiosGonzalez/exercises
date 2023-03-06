dc6662f031f10267542168a38abe9a7aff18dbc5
José Antonio Ríos González <116028548+JoseAntonioRiosGonzalez@users.noreply.github.com>
2023-03-06 17:38:47 +0100

    Add files via upload

---
+++ java/HelloWorld.java
@@ -0,0 +1,17 @@
+/**
+ * @author jose rios
+ *
+ * Main class application entry point
+ */
+public class HelloWorld {
+    /**
+     *
+     * Main function entry point for the application
+     * @param args argument to receive
+     * @since 1.0 // @since is used to indicate which version added the class, method, etc.
+     * //if we included @version, this would indicate the current version
+     **/
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
