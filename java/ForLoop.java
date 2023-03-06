383b10a4486d995b98235ab477e1888dccc69b36
José Antonio Ríos González <116028548+JoseAntonioRiosGonzalez@users.noreply.github.com>
2023-03-06 17:43:14 +0100

    Add files via upload

---
+++ java/ForLoop.java
@@ -0,0 +1,13 @@
+public class ForLoop {
+
+    public static void main(String[] args){
+
+        //1-starting condition
+        //2-stop condition
+        //3-step condition
+        for (int i =0; i <=10; i++){//normally we use for loop when we already know
+        //how many iterations there will be (this is called a determined control structure)
+            System.out.println(i);
+        }
+    }
+}
