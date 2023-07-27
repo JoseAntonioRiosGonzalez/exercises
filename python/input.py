c630fc6813ee4e38f9f6a2ad816f786ea6e174f8
José Antonio Ríos González <116028548+JoseAntonioRiosGonzalez@users.noreply.github.com>
2023-07-27 23:06:39 +0200

    Add files via upload

--- python/input.py
+++ python/input.py
@@ -1,3 +1,7 @@
+""" STATEMENT: Create a Python program where the user will be asked to enter their first name and last name. 
+Finally, the program will print a welcome message on the screen, along with their first name and last name."""
+
+
 name= input("Please enter your name: ")
 surname= input("Please enter your surname: ")
 
