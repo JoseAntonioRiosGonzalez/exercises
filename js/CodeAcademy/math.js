3d0da8707534ee3f5a2449bb01f8c869162bc147
José Antonio Ríos González <116028548+JoseAntonioRiosGonzalez@users.noreply.github.com>
2024-01-27 16:26:59 +0100

    Add files via upload

---
+++ js/CodeAcademy/math.js
@@ -0,0 +1,4 @@
+console.log(Math.random());//prints random number betw 0-1
+console.log(Math.random() * 50);//number betw 0-50(decimal)
+console.log(Math.floor(Math.random() * 50));//same no decimals
+console.log(Math.ceil(43.8));
---
+++ js/CodeAcademy/method.js
@@ -0,0 +1,4 @@
+console.log('hello'.toUpperCase());
+console.log('Hey'.startsWith('H'));
+console.log('    Delete spaces   '.trim());
+console.log(Number.isInteger(45));
