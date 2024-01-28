a0b698d3d57aaca0608af2cc17f76479c2c05ab4
José Antonio Ríos González <116028548+JoseAntonioRiosGonzalez@users.noreply.github.com>
2024-01-29 00:51:23 +0100

    Add files via upload

---
+++ c#/Microsoft/type/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("String");// for String
+Console.WriteLine('c');// for char
+Console.WriteLine(123);
+Console.WriteLine(0.25F);// float
+Console.WriteLine(0.25676);// double
+Console.WriteLine(0.4564523452M);// decimal
+Console.WriteLine(true);
+
+char caracter= 'b';
+Console.WriteLine(caracter);
+
+string name= "Luis";
+int gameScore= 1200;
+decimal particlesPerMillion= 23.58349839484M;
+bool isMarried= false;
+var message= "Nice to see u!";//implicitly typed local variable.
+//the type of the local variable is obtained from the expression used to initialize the variable
+//always var variable has to be initialized
+
+//  var delivery; //error!!
+
---
+++ c#/Microsoft/write/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello World!");
+Console.Write("In this way ");
+Console.Write("u'll have everything ");
+Console.Write("in the same line ");
